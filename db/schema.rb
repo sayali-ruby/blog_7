8d078f76e5189c24f2dc440545d2c4b36624eb06
Sayali <s22404923@gmail.com>
2021-12-25 22:28:51 +0530

    modified categories

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_22_052522) do
+ActiveRecord::Schema.define(version: 2021_12_24_163141) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "sports", force: :cascade do |t|
     t.string "name"
